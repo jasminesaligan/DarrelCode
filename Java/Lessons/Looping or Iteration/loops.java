d9390eb590638441affad21d94fedad9eee63d8b
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2023-09-11 14:16:50 +0800

    df

---
+++ Java/Lessons/Looping or Iteration/loops.java
@@ -0,0 +1,28 @@
+public class loops {
+    public static void main(String[] args) throws Exception {
+
+    //sheesh is a variable so you can change it
+    //for loop
+        for (int sheesh = 5; sheesh != 0; sheesh--) {
+            System.out.println("John Darrel");
+        }
+        
+    //palayaw is a variable so you can change it
+    //do while loop
+         int palayaw = 5;
+
+        do {
+            System.out.println("Poquiz");
+            palayaw--;
+        } while (palayaw != 0);
+        
+        
+    //while loop    
+        int surnameITO= 5;
+
+        while (surnameITO != 0) {
+            System.out.println("alyas legendary master");
+            surnameITO--;
+        }
+    }
+}
